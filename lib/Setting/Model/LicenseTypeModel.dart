d2f76d7dc5adaaeb9380baa668cda988d243266d
Beacon <haidang2403@gmail.com>
2023-03-30 10:55:17 +0700

    no message

--- lib/Setting/Model/LicenseTypeModel.dart
+++ lib/Setting/Model/LicenseTypeModel.dart
@@ -3,20 +3,29 @@ class LicenseTypeModel {
   int id = 0 ;
   String name = '';
   String comment = '';
-  int count = 0;
+  int testcount = 0;
+  int questionCount = 0;
+  int minutes = 0;
+  int require = 0;
 
   LicenseTypeModel({
     required this.id,
     required this.name,
     required this.comment,
-    required this.count,
+    required this.testcount,
+    required this.questionCount,
+    required this.minutes,
+    required this.require,
   });
 
   LicenseTypeModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     name = json['name'];
     comment = json['comment'];
-    count = json['count'];
+    testcount = json['testcount'];
+    questionCount = json['questionCount'];
+    minutes = json['minutes'];
+    require = json['require'];
   }
 
   Map<String, dynamic> toJson() {
@@ -24,7 +33,10 @@ class LicenseTypeModel {
     data['id'] = id;
     data['name'] = name;
     data['comment'] = comment;
-    data['count'] = count;
+    data['testcount'] = testcount;
+    data['questionCount'] = questionCount;
+    data['minutes'] = minutes;
+    data['require'] = require;
 
     return data;
   }
